f83baab106a65bff08073ef4173018f8a00a8ee8
Manuel <97695681+manudelu@users.noreply.github.com>
2023-12-24 14:22:46 +0000

    Add files via upload

--- Assignment 3/ex1.m
+++ Assignment 3/ex1.m
@@ -1,4 +1,8 @@
 %% Modelling and Control of Manipulator assignment 3 - Exercise 1: Jacobian matrix
+clc;
+clear;
+close("all");
+addpath('include');
 
 % The same model of assignment 2
 geom_model = BuildTree();
@@ -7,11 +11,19 @@ linkType = zeros(numberOfLinks,1); % specify two possible link type: Rotational,
 bTi = zeros(4,4,numberOfLinks);% Trasformation matrix i-th link w.r.t. base
 
 % Initial joint configuration 
-q = [1.3,1.3,1.3,1.3,1.3,1.3,1.3];
+q1 = [1.8,1.8,1.8,1.8,1.8,1.8,1.8];
+q2 = [0.3, 1.4, 0.1, 2.0, 0, 1.3, 0];
+q3 = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0];
+q4 = [1, 1, 1, 1, 1, 1, 1];
+q = [q1', q2', q3', q4'];
 
-%% Compute direct geometry
+for n=1:4
+    % Compute direct geometry
+    biTei = GetDirectGeometry(q(:,n), geom_model, linkType);
+    % Compute the transformation w.r.t. the base
+    bTe = GetTransformationWrtBase(biTei, numberOfLinks);
+    % Compute the Jacobian matrices 
+    J(:,:,n) = GetJacobian(biTei, bTe, linkType); 
+end
 
-% Compute the transformation w.r.t. the base
-
-% Computing end effector jacobian 
-
+J(:,:,:)
