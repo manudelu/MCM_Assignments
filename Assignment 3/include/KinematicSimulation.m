3b3fdab1356a1e3ac265f0fd8d14bc8150440947
Manuel <97695681+manudelu@users.noreply.github.com>
2023-12-24 16:29:53 +0100

    Update KinematicSimulation.m

--- Assignment 3/include/KinematicSimulation.m
+++ Assignment 3/include/KinematicSimulation.m
@@ -11,9 +11,17 @@ function [q] = KinematicSimulation(q, q_dot, ts, q_min, q_max)
 % Outputs
 % - q new joint configuration
 
-
-    % Updating q
-
-    % Saturating the joint velocities 
+    % Update the joint configuration based on the velocities and time step
+    % (basic forward kinematics integration step)
+    q = q + q_dot*ts;
+    
+    % Saturate the joint angles to ensure they stay within the defined limits
+    for i=1:length(q)
+        if q(i) < q_min(i)
+            q(i) = q_min(i);
+        elseif q(i) > q_max(i)
+            q(i) = q_max(i);
+        end
+    end
 
 end
